f82a4b1ff6b095274c54524f587b8fa735421d03
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-20 17:59:26 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,11 +1,11 @@
 <?php
 /* setting bot */
-$baca_plus=$biru."
-──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$biru
-──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$biru
+$baca_plus=$putih."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$putih
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$putih
 ──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
-creator:$ijo Per-mana$putih || code invite:$ijo F4dwBcH9YC
-chanel :$kuning https://youtube.com/permana29$putih".$t;
-$msg_baca= "[•] Official Chanel Youtube Per-mana".$t$t."[•] info bagi yang mengalami kesalahan saat login".$t."[•] silahkan download ulang skrip".$t."[•] kesalahan terdapat pada versi os lolipop";
+creator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x$putih
+chanel :$kuning https://youtube.com/permana29 || JANGAN LUPA:$ijo SUBSCRIBE".$t;
+$msg_baca= "[•] Official Chanel Youtube Per-mana".$t."[•] info bagi yang mengalami kesalahan saat login".$t."[•] silahkan download ulang skrip".$t."[•] kesalahan terdapat pada versi os lolipop";
 $stat_baca=true;
 ?>
