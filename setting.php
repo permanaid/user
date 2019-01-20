539b8ddaa5397435d9d86d984853e45718966aeb
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-20 18:09:02 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -4,8 +4,8 @@ $baca_plus=$merah."
 ──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$merah █▀▄ █── █─█ ▄▀▀────$merah
 ──── █▀█ █▀█ █─ █▀█────$hijau █─█ █─▄ █─█ ─▀▄────$merah
 ──── ▀▀─ ▀─▀ ─▀ ▀─▀────$hijau █▀─ ▀▀▀ ─▀─ ▀▀─────$merah
-creator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x$putih
-chanel :$kuning {*} https://youtube.com/permana29 {*}$merah JANGAN LUPA: SUBSCRIBE".$t;
+creator:$ijo PERMANA$putih || code invite:$ijo F9NAmmRD7x$putih
+chanel :$kuning {*} https://youtube.com/permana29 {*}$putih JANGAN LUPA: SUBSCRIBE".$t;
 $msg_baca= "[•] Official Chanel Youtube Per-mana".$t."[•] info bagi yang mengalami kesalahan saat login".$t."[•] silahkan download ulang skrip".$t."[•] kesalahan terdapat pada versi os lolipop";
 $stat_baca=true;
 ?>
