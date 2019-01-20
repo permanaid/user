95ef57889d878513c4b54e1908d8ed7711518eba
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-20 17:54:06 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -5,7 +5,7 @@ $baca_plus=$biru."
 ──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$biru
 ──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
 creator:$ijo Per-mana$putih || code invite:$ijo F4dwBcH9YC
-chanel :$kuning https://youtube.com/permana29$putih Jangan Lupa SUBSCRIBE".$t;
+chanel :$kuning https://youtube.com/permana29".$t;
 $msg_baca= "[•] Official Chanel Youtube Per-mana".$t$t."[•] info bagi yang mengalami kesalahan saat login".$t."[•] silahkan download ulang skrip".$t."[•] kesalahan terdapat pada versi os lolipop";
 $stat_baca=true;
 ?>
