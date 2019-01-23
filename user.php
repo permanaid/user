328cee5de9cc2dab3b8618f20b9e7ff1370f0ed7
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-24 00:40:20 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,6 +1,6 @@
 <?php
 $stat_sseth=true;
-$msgsseth= "[---] enjoy your life";
+$msgsseth= "[---] No tuyul no money";
 $stat_cp = true;
 $msgcp = "[--] enjoy your life";
 $figlet= $ijo."
@@ -8,7 +8,7 @@ $figlet= $ijo."
 ── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." ['''  || \  / _    _.       
 ── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." |'' L|||  \/ (/_|`_\|".$ijo."
 ── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
-  $putih."[-]creator:".$ijo." Per-mana   ".$t.
-  $putih."[-]ide    :".$kuning." Subscriber  ".$t.
-  $putih."[-]chanel :".$ijo." https://youtube.com/permana29 ".$t.
-  $putih."[-]invite :".$ijo." uiemj8     ".$t;
+  $putih."[+] creator:".$ijo." Per-mana   ".$t.
+  $putih."[+] ide    :".$kuning." Subscriber  ".$t.
+  $putih."[+] chanel :".$ijo." https://youtube.com/permana29 ".$t.
+  $putih."[+] invite :".$ijo." uiemj8     ".$t;
