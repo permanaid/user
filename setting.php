ccd105d13c56c44e4137743603d4377a9c844862
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-23 12:26:40 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -14,7 +14,7 @@ $mc=$kuning."
 ──── █─█─█ █─█ █─▀█ █▀▀ ──█────── █─ █─█ █▀█ █▀▀────
 ──── ▀───▀ ─▀─ ▀──▀ ▀▀▀ ──▀────── ─▀ ─▀─ ▀▀─ ▀▀▀────
 ".$pth."creator: ".$ijo."Permana ".$pth."|| ".$ijo."subcribe chanel Per-mana".$t.$pth."invite : ".$ijo."BJ67A
-".$pth."info   : ".$pink."Jangan Lupa Subscribe My chanel Per-mana".$t;
+".$pth."info   : ".$ijo."Jangan Lupa Subscribe My chanel Per-mana".$t;
 $msg_mc=$ijo."[•] enjoy your life";
 $stat_mc=true;
 
