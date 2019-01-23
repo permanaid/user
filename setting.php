e252cfc0fb66a574f99e3b6a333911b7301f66f0
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-23 11:37:30 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -13,7 +13,7 @@ $ccu= $red."
 ──── ▄▀ ▄▀▄ ▀ █▄─█──── ▄▀ █── █─█ █▀▄────".$pth."
 ──── █─ █─█ █ █─▀█──── █─ █─▄ █─█ █▀█────
 ──── ─▀ ─▀─ ▀ ▀──▀──── ─▀ ▀▀▀ ─▀─ ▀▀─────
-creator: ".$ijo."Permana ".$pth."|| chanel: ".$ijo."Per-mana".$t;
-$msg_ccu=$ijo."[•] Jangan Lupa Subscribe Chanel : Per-mana";
+creator: ".$ijo."adidoank ".$pth."|| chanel: ".$ijo."adi bordir".$t;
+$msg_ccu=$ijo."[•] official chanel adi bordir dan sungging";
 $stat_ccu=true;
 ?>
