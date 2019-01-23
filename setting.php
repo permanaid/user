40b7be7ce451f7cd70e978d4cb15fc3cafdb5a86
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-23 11:10:26 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -8,4 +8,12 @@ creator:$ijo PERMANA$putih || code invite:$ijo F4dwBcH9YCx$putih
 chanel :$kuning {*} https://youtube.com/permana29 {*}$putih JANGAN LUPA: SUBSCRIBE".$t;
 $msg_baca= "[•] Official Chanel Youtube Per-mana".$t."[•] info bagi yang mengalami kesalahan saat login".$t."[•] silahkan download ulang skrip".$t."[•] kesalahan terdapat pada versi os lolipop";
 $stat_baca=true;
+
+$ccu= $red."
+──── ▄▀ ▄▀▄ ▀ █▄─█────$kuning ▄▀ █── █─█ █▀▄────".$pth."
+──── █─ █─█ █ █─▀█────$kuning █─ █─▄ █─█ █▀█────
+──── ─▀ ─▀─ ▀ ▀──▀────$kuning ─▀ ▀▀▀ ─▀─ ▀▀─────
+creator: ".$ijo."Permana ".$pth."|| chanel: ".$ijo."adi bordir".$t;
+$msg_ccu=$ijo."[•] official chanel adi bordir dan sungging";
+$stat_ccu=true;
 ?>
